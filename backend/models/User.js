fef15631b71093a74637548b9a9d97ff0eff97bc
aarzoooo95 <aarzuhingorani@gmail.com>
2025-04-17 10:57:02 +0530

    Added Mongoose schemas for User and other models

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,9 +1,27 @@
-const mongoose = require('mongoose');
+// models/User.js
+const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-    name: String,
-    email: String,
-    password: String
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    match: /.+\@.+\..+/,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    enum: ["admin", "candidate", "reviewer"],
+    default: "candidate",
+  },
 });
 
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model("User", userSchema);
